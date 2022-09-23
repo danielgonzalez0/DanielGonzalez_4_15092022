4190b6521f15e28e7cf802ff426de371560dc050
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-09-23 17:29:44 +0200

    mise en place fermeture modal + correction mineure css

--- Landing_Page_version_finale/modal.js
+++ Landing_Page_version_finale/modal.js
@@ -1,23 +1,40 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
 
+//code added by Daniel Gonzalez (september 2022)
 
+//----------------- Close modal form --------------------//
+
+//variable declaration
+const modalCross = document.querySelector('.close');
+
+//catch user event & setting up interactions
+modalCross.addEventListener('click', closeModal);
+
+// function => close modal form
+function closeModal() {
+  modalbg.style.display = 'none';
+
+  //-----------------end  Close modal form --------------//
+}
+
+//end code added by Daniel Gonzalez
