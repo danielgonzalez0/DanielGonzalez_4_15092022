def4676ab3a03f37e33eb8a88a3e2c597d6c98d8
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-09-24 10:25:36 +0200

    amélioration fermeture modal avec animation

--- Landing_Page_version_finale/modal.js
+++ Landing_Page_version_finale/modal.js
@@ -11,18 +11,20 @@ function editNav() {
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
+const modalContent = document.querySelector('.content'); //added by Daniel Gonzalez
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
+  modalContent.classList.remove('modalClosed'); // add by Daniel Gonzalez (september 2022)
   modalbg.style.display = 'block';
 }
 
 //code added by Daniel Gonzalez (september 2022)
 
-//----------------- Close modal form --------------------//
+//-------------- Close modal form ----------------//
 
 //variable declaration
 const modalCross = document.querySelector('.close');
@@ -32,9 +34,12 @@ modalCross.addEventListener('click', closeModal);
 
 // function => close modal form
 function closeModal() {
-  modalbg.style.display = 'none';
+  modalContent.classList.add('modalClosed');
+  setTimeout(function () {
+    modalbg.style.display = 'none';
+  }, 750);
 
-  //-----------------end  Close modal form --------------//
+  //------------end  Close modal form -----------//
 }
 
 //end code added by Daniel Gonzalez
