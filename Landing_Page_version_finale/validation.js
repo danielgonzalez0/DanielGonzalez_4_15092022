9083d6f147f981e5e601d4c89b5dea3ccba67b66
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-09-28 18:36:34 +0200

    mise en place validation données à la soumission du formulaire => validation auto navigateur

--- Landing_Page_version_finale/validation.js
+++ Landing_Page_version_finale/validation.js
@@ -1,14 +1,30 @@
 //DOM Elements Selection
 const contactForm = document.getElementById('frmContact');
 const successMessage = document.querySelector('.succes-confirmation');
-console.log(contactForm);
+const fields = document.querySelectorAll('input[required]');
+console.log(fields);
 
-//event listener
+//event listener & step process
 
-// on form submit
+// -----on form submit--------
 contactForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault(); //submit behavior disabling
+  let valid = true;
+  //loop to check validity of each field required
+  fields.forEach((field) => {
+    if (!validateField(field)) {
+      valid = false;
+    }
+    //console check for manual test
+    console.log(`-----field: ${field.id}-----`);
+    console.log(`valid = ${valid}`);
+  });
+  if (valid) {
+    validate();
+  }
 });
+
+//---------------------------functions------------------------------
 
 /**
  * function validate => show success message on form submit
@@ -19,3 +35,20 @@ function validate() {
   console.log(successMessage);
   console.log(contactForm);
 }
+
+/**
+ * function validateField => check validity of required field & return true
+ */
+function validateField(field) {
+  if (field.checkValidity()) {
+    return true;
+  } else {
+    field.parentElement.setAttribute('data-error-visible', 'true');
+    field.parentElement.setAttribute('data-error', field.validationMessage);
+    return false;
+  }
+}
+
+/**
+ * 
+ */
