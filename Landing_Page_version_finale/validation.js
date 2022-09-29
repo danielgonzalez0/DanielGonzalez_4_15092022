83c91e7e0d2be82f572b57bb6f5cd8c3536a4f2b
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-09-29 17:24:31 +0200

    ajout conservation données formulaire si non données non valides

--- Landing_Page_version_finale/validation.js
+++ Landing_Page_version_finale/validation.js
@@ -1,15 +1,49 @@
+//Variable
+let valid = true;
+
 //DOM Elements Selection
 const contactForm = document.getElementById('frmContact');
 const successMessage = document.querySelector('.succes-confirmation');
 const fields = document.querySelectorAll('input[required]');
-console.log(fields);
+const inputFormValue = document.querySelectorAll(
+  'input[type=text], input[type=email], input[type=date],input[type=number]'
+);
+const inputFormRadio = document.querySelectorAll('input[type=radio]');
+const inputCheckbox2 = document.getElementById('checkbox2');
+console.log(inputCheckbox2.checked);
+
+//
+//contactForm.setAttribute('novalidate', true);
 
 //event listener & step process
+
+// -----on form field focus & blur--------
+fields.forEach((field) => {
+  field.addEventListener('focus', () => {
+    resetErrorMessage(field);
+  });
+});
+
+fields.forEach((field) => {
+  field.addEventListener('click', () => {
+    resetErrorMessage(field);
+  });
+});
+
+fields.forEach((field) => {
+  field.addEventListener('blur', () => {
+    validateField(field);
+  });
+});
 
 // -----on form submit--------
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault(); //submit behavior disabling
-  let valid = true;
+
+  // reset field error message & variable valid
+  fields.forEach((field) => {
+    resetErrorMessage(field);
+  });
   //loop to check validity of each field required
   fields.forEach((field) => {
     if (!validateField(field)) {
@@ -18,9 +52,11 @@ contactForm.addEventListener('submit', (e) => {
     //console check for manual test
     console.log(`-----field: ${field.id}-----`);
     console.log(`valid = ${valid}`);
+    console.log(field.value);
   });
   if (valid) {
     validate();
+    resetInputForm();
   }
 });
 
@@ -32,8 +68,7 @@ contactForm.addEventListener('submit', (e) => {
 
 function validate() {
   successMessage.classList.replace('select-hide', 'success-show');
-  console.log(successMessage);
-  console.log(contactForm);
+  //next steps not coding yet
 }
 
 /**
@@ -50,5 +85,31 @@ function validateField(field) {
 }
 
 /**
- * 
+ * function resetErrorMessage => reset error message of a form field & the variable valid
  */
+
+function resetErrorMessage(field) {
+  if ((field.parentElement.classList.value = 'formData')) {
+    field.parentElement.removeAttribute('data-error-visible', 'true');
+    field.parentElement.removeAttribute('data-error', field.validationMessage);
+  }
+  valid = true;
+
+  //console check for manual test
+  /* console.log(`----resetField check-----`);
+  console.log(`----field.valid true : ${field.valid}-----`);*/
+}
+
+/**
+ * function resetInputForm => reset input value manually
+ */
+
+function resetInputForm() {
+  inputFormValue.forEach((input) => {
+    input.value = '';
+  });
+  inputFormRadio.forEach((input) => {
+    input.checked = false;
+  });
+  inputCheckbox2.checked = false;
+}
