8baf29627034dfbcb6cf08b665df8c6739a9ad14
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-10-12 17:53:10 +0200

    installation eslint + correction erreur

--- Landing_Page_version_finale/modal.js
+++ Landing_Page_version_finale/modal.js
@@ -1,41 +1,41 @@
 function editNav() {
-  var x = document.getElementById('myTopnav');
-  if (x.className === 'topnav') {
+  const x = document.getElementById('myTopnav');
+  const y = document.querySelector('.main-navbar-container');
+  if (x.className === 'topnav' && window.innerWidth < 875) {
     x.className += ' responsive';
+    y.classList.add('menuactive');
   } else {
     x.className = 'topnav';
+    y.classList.replace('menuactive', 'menuinactive');
+    setTimeout(() => {
+      y.classList.remove('menuinactive');
+    }, 900);
   }
 }
 
 // DOM Elements
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
-const formData = document.querySelectorAll('.formData');
 const modalContent = document.querySelector('.content');
 const fields = document.querySelectorAll('input[required], #checkbox1');
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
   modalContent.classList.remove('modalClosed');
   modalbg.classList.remove('select-hide');
 }
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
-//-------------- Close modal form ----------------//
-
-//variable declaration
+// -------------- Close modal form ----------------
+// variable declaration
 const modalCross = document.querySelector('.close');
-
-//catch user event & setting up interactions
-modalCross.addEventListener('click', closeModal);
-
 /**
  * function => close modal form
  */
 function closeModal() {
   modalContent.classList.add('modalClosed');
-  setTimeout(function () {
+  setTimeout(() => {
     modalbg.classList.add('select-hide');
     successMessage.classList.replace('success-show', 'select-hide');
     fields.forEach((field) => {
@@ -44,4 +44,6 @@ function closeModal() {
     inputCheckbox1.checked = true;
   }, 750);
 }
-//------------end  Close modal form -----------//
+// catch user event & setting up interactions
+modalCross.addEventListener('click', closeModal);
+// ------------end  Close modal form -----------
--- Landing_Page_version_finale/validation.js
+++ Landing_Page_version_finale/validation.js
@@ -1,13 +1,13 @@
-//DOM Elements Selection
+// DOM Elements Selection
 
-//Array elements
-const contactForm = document.forms['reserve'];
+// Array elements
+const contactForm = document.querySelector('#frmContact');
 const inputFormRadio = document.querySelectorAll('input[name=location]');
 const inputFormValue = document.querySelectorAll(
-  'input[type=text], input[type=email], input[type=date],input[type=number]'
+  'input[type=text], input[type=email], input[type=date], input[type=number]',
 );
 
-//single Element
+// single Element
 const successMessage = document.querySelector('.succes-confirmation');
 const inputFirstName = document.querySelector('#first');
 const inputLastName = document.querySelector('#last');
@@ -17,7 +17,7 @@ const inputNumber = document.querySelector('#quantity');
 const inputCheckbox1 = document.querySelector('#checkbox1');
 const inputCheckbox2 = document.getElementById('checkbox2');
 
-//----------------------functions-------------------------
+// ----------------------functions-------------------------
 /**
  * function validate => show success message on form submit
  */
@@ -25,7 +25,7 @@ function validate() {
   successMessage.classList.replace('select-hide', 'success-show');
 }
 
-//error message display
+// error message display
 
 /**
  * add error message of a form field
@@ -49,69 +49,68 @@ function removeErrorMessage(field) {
  * reset input value manually
  */
 function resetInputForm() {
-  inputFormValue.forEach((input) => {
-    input.value = '';
+  inputFormValue.forEach((field) => {
+    field.value = '';
   });
-  inputFormRadio.forEach((input) => {
-    input.checked = false;
+  inputFormRadio.forEach((field) => {
+    field.checked = false;
   });
   inputCheckbox1.checked = true;
   inputCheckbox2.checked = false;
 }
 
-//specific functions
+// specific functions
 /**
  * check validity of input value type = text
  * @param {Object} input - the form field to be controlled => must be a text type
  */
 function nameValidity(input) {
-  let regexName = /^[-a-zA-ZÀ-ÿ \']{2,30}$/;
-  let errorMessage = 'Veuillez entrer 2 caractères ou plus pour ce champ.';
+  const regexName = /^[-a-zA-ZÀ-ÿ ']{2,30}$/;
+  const errorMessage = 'Veuillez entrer 2 caractères ou plus pour ce champ.';
   if (!regexName.test(input.value)) {
     addErrorMessage(input, errorMessage);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
+
 /**
  * check validity of inputs type = email
  * @param {Object} input - the form field to be controlled => must be a email type
  */
 function emailValidity(input) {
-  let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; //https://regexr.com/3e48o
-  let errorMessage = 'Veuillez entrer une adresse mail valide';
+  const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; // https://regexr.com/3e48o
+  const errorMessage = 'Veuillez entrer une adresse mail valide';
   if (!regexEmail.test(input.value)) {
     addErrorMessage(input, errorMessage);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
+
 /**
  * check if a date is valid
  * @param {Object} input - the form field to be controlled => must be a date type
  */
 function dateValidity(input) {
-  let errorMessage = 'Veuillez entrer une date valide';
+  const errorMessage = 'Veuillez entrer une date valide';
   const date = new Date(input.value);
-  if (isNaN(date.getTime())) {
+  if (Number.isNaN(date.getTime())) {
     addErrorMessage(input, errorMessage);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
+
 /**
  * check user birth date validity
  * @param {Object} input - the form field to be controlled => must be a date type
  */
 function ageValidity(input) {
-  let errorMessage1 =
-    'Vous ne pouvez pas entrer une date inférieure au 01/01/1900';
-  let errorMessage2 = 'Vous devez avoir 18 ans mininum pour vous inscrire';
+  const errorMessage1 = 'Vous ne pouvez pas entrer une date inférieure au 01/01/1900';
+  const errorMessage2 = 'Vous devez avoir 18 ans mininum pour vous inscrire';
   const date = new Date(input.value);
-  const minDate = new Date(1899, 12, 01);
+  const minDate = new Date(1899, 12, 1);
   const today = new Date();
   const minAgeYear = today.getFullYear() - 18;
   const minAgeMonth = today.getMonth();
@@ -120,12 +119,12 @@ function ageValidity(input) {
   if (date < minDate) {
     addErrorMessage(input, errorMessage1);
     return false;
-  } else if (date > minAgeDate) {
+  }
+  if (date > minAgeDate) {
     addErrorMessage(input, errorMessage2);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 /**
@@ -133,49 +132,48 @@ function ageValidity(input) {
  * @param {Object} input - the form field to be controlled => must be a number type
  */
 function numberValidity(input) {
-  let errorMessage = 'Veuillez entrer un chiffre compris entre 0 et 99';
-  let number = input.value;
-  if (isNaN(number) || number < 0 || number > 99 || number === '') {
+  const errorMessage = 'Veuillez entrer un chiffre compris entre 0 et 99';
+  const number = input.value;
+  if (Number.isNaN(number) || number < 0 || number > 99 || number === '') {
     addErrorMessage(input, errorMessage);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
+
 /**
  * check validity of the input type = radio
  * @param {Object} input - the form field to be controlled => must be a radio type
  */
 function radioValidity(input) {
   let radioIschecked = false;
-  let errorMessage = 'Vous devez cocher un choix pour continuer.';
+  const errorMessage = 'Vous devez cocher un choix pour continuer.';
   input.forEach((field) => {
     if (field.checked) {
-      return (radioIschecked = true);
+      radioIschecked = true;
     }
   });
   if (!radioIschecked) {
     addErrorMessage(input[0], errorMessage);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
+
 /**
  * check validity of the input checkbox1
  * @param {Object} input - the form field to be controlled => must be a checkbox type
  */
 function checkbox1Validity(input) {
   if (!input.checked) {
-    let errorMessage = 'Vous devez accepter les termes et conditions.';
+    const errorMessage = 'Vous devez accepter les termes et conditions.';
     addErrorMessage(input, errorMessage);
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
-//global functions
+// global functions
 /**
  * check if a form input is valid
  * @param {Object} input - the form field to be controlled
@@ -186,25 +184,18 @@ function validateInput(input, type) {
   switch (type) {
     case 'name':
       return nameValidity(input);
-      break;
     case 'email':
       return emailValidity(input);
-      break;
     case 'date':
       return dateValidity(input);
-      break;
     case 'number':
       return numberValidity(input);
-      break;
     case 'radio':
       return radioValidity(input);
-      break;
     case 'checkbox':
       return checkbox1Validity(input);
-      break;
     default:
       return false;
-      break;
   }
 }
 
@@ -212,41 +203,40 @@ function validateInput(input, type) {
  * check the validity of the form
  */
 function validateForm() {
-  let checkInputFirstName = validateInput(inputFirstName, 'name');
-  let checkInputLastName = validateInput(inputLastName, 'name');
-  let checkInputEmail = validateInput(inputEmail, 'email');
-  let checkInputDate = validateInput(inputDate, 'date');
-  let checkAgeUser = ageValidity(inputDate);
-  let checkInputNumber = validateInput(inputNumber, 'number');
-  let CheckInputCheckbox = validateInput(inputCheckbox1, 'checkbox');
-  let checkInputRadio = validateInput(inputFormRadio, 'radio');
+  const checkInputFirstName = validateInput(inputFirstName, 'name');
+  const checkInputLastName = validateInput(inputLastName, 'name');
+  const checkInputEmail = validateInput(inputEmail, 'email');
+  const checkInputDate = validateInput(inputDate, 'date');
+  const checkAgeUser = ageValidity(inputDate);
+  const checkInputNumber = validateInput(inputNumber, 'number');
+  const CheckInputCheckbox = validateInput(inputCheckbox1, 'checkbox');
+  const checkInputRadio = validateInput(inputFormRadio, 'radio');
 
   if (
-    checkInputFirstName &&
-    checkInputLastName &&
-    checkInputEmail &&
-    checkInputDate &&
-    checkAgeUser &&
-    checkInputNumber &&
-    CheckInputCheckbox &&
-    checkInputRadio
+    checkInputFirstName
+    && checkInputLastName
+    && checkInputEmail
+    && checkInputDate
+    && checkAgeUser
+    && checkInputNumber
+    && CheckInputCheckbox
+    && checkInputRadio
   ) {
-    //coding post steps process
+    // coding post steps process
     validate();
     resetInputForm();
   }
 }
 
-//--------------event listener-----------------
+// --------------event listener-----------------
 
-//on form submit
-// console.log(contactForm);
+// on form submit
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
   validateForm();
 });
 
-//on form field focus
+// on form field focus
 inputFirstName.addEventListener('focus', () => {
   removeErrorMessage(inputFirstName);
 });
@@ -263,7 +253,7 @@ inputNumber.addEventListener('focus', () => {
   removeErrorMessage(inputNumber);
 });
 
-//on form field click{
+// on form field click{
 inputFormRadio.forEach((field) => {
   field.addEventListener('click', () => {
     removeErrorMessage(field);
@@ -273,8 +263,7 @@ inputCheckbox1.addEventListener('click', () => {
   removeErrorMessage(inputCheckbox1);
   validateInput(inputCheckbox1, 'checkbox');
 });
-
-//on form field blur
+// on form field blur
 inputFirstName.addEventListener('blur', () => {
   validateInput(inputFirstName, 'name');
 });
